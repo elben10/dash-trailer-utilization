e5b9d6e925192c0f62045c210bc39f247883c13b
Jakob Jul Elben <elbenjakobjul@gmail.com>
2019-04-12 14:52:39 +0200

    ...

--- app.py
+++ app.py
@@ -2,19 +2,29 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dashmaterialui as dmui
+import flask
 
-external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
-
-app = dash.Dash(__name__, external_stylesheets=[])
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, server=server)
 
 app.layout = html.Div(
     [
         dmui.AppBar(
             dmui.Toolbar(dmui.Typography("DSV", variant="h2", color="inherit"))
         ),
-        html.Main([dmui.Toolbar(), dmui.Grid(container=True, children=[dmui.Grid(item=True, children=[dmui.Typography('HELLO')])])]),
+        html.Main(
+            [
+                dmui.Toolbar(),
+                dmui.Grid(
+                    container=True,
+                    children=[
+                        dmui.Grid(item=True, children=[dmui.Typography("HELLO")])
+                    ],
+                ),
+            ]
+        ),
     ]
 )
 
-if __name__ == "__main__":
-    app.run_server(debug=True)
+# if __name__ == "__main__":
+#     app.run_server()
